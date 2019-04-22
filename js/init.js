a6ad7e6249fc4ee4153dadcb363b62e79ab584be
Craig Huff <craig@craighuff.com>
2019-04-21 18:30:45 -0700

    first commit

---
+++ js/init.js
@@ -0,0 +1,44 @@
+(function($){
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
+
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$('.materialboxed').on("click", function(){
+
+    var imgSrc = $(this).prop("src");
+
+    var actualImg = new Image();
+
+    actualImg.src = imgSrc;
+
+    var colorThief = new ColorThief();
+    var newColor = colorThief.getColor(actualImg);
+    newColor = "rgb("+ newColor[0] + "," + newColor[1] + "," + newColor[2] + ")";
+    console.log("URL", imgSrc);
+
+    console.log("Waiting...");
+
+    setTimeout(function() {
+        $("div#materialbox-overlay").css("background-color", newColor);
+    }, 10);
+
+});
+
+function showLogs() {
+    if (iframe3 === 'block') {
+        iframe3.style.display = 'none';
+    }
+    else {
+        iframe3.style.display = 'block';
+    }
+    console.log("Logs runs ");
+}
+
+
